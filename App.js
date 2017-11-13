1c8c052e238bd7c3a76a2db79ae5b2c6d68cf035
coej94 <coej94@hotmail.com>
2017-11-13 18:34:52 +0100

    styling added to tabbar

--- App.js
+++ App.js
@@ -1,14 +1,44 @@
 import React from 'react';
-import {View, Text, Image} from 'react-native';
-import { TabNavigator } from 'react-navigation';
-import {Home, Favorites, Categories, Plan, Settings} from './src/screens'
+import {
+    TabNavigator
+} from 'react-navigation';
+import {
+    Home,
+    Favorites,
+    Categories,
+    Plan,
+    Settings
+} from './src/screens';
 
 const App = TabNavigator({
-    Home: { screen: Home },
-    Favorites: { screen: Favorites },
-    Categories: { screen: Categories },
-    Plan: { screen: Plan },
-    Settings: { screen: Settings },
+        Home: {
+            screen: Home
+            
+        },
+        Favorites: {
+            screen: Favorites
+        },
+        Categories: {
+            screen: Categories
+        },
+        Plan: {
+            screen: Plan
+        },
+        Settings: {
+            screen: Settings
+        },
+    },
+
+    {
+    tabBarOptions: {
+        labelStyle: {
+            fontSize: 12,
+        },
+        style: {
+            backgroundColor: '#181818',
+        },
+        activeTintColor: '#980c0c'
+    }
 });
 
 export default App;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,12 +3,25 @@ import { View, Text } from 'react-native';
 
 const Home = () => {
   return (
-    <View>
-      <Text>
-       Hjem!
+    <View style={styles.container}>
+      <Text style={styles.city}>
+        København
       </Text>
     </View>
   );
+};
+
+const styles = {
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    backgroundColor: '#242424'
+  },
+  city: {
+    color: '#f4f4f4',
+    fontSize: 50,
+    marginTop: 40
+  }
 };
 
 export { Home };
